f159fab70d5fccf6ee824c4ea1378b82e8eb23c1
Devon Wells <devon.wellsa@gmail.com>
2023-04-27 10:12:21 -0400

    feat: update tests

--- src/lib/repositoryScanner/repositoryScanner.spec.ts
+++ src/lib/repositoryScanner/repositoryScanner.spec.ts
@@ -12,19 +12,40 @@ describe("RepositoryScanner", () => {
     const rootDir = path.resolve(__dirname, "../../../__mocks__/TestRepo");
     const components = await repositoryScanner.scanRepository(rootDir);
 
-    console.log(components);
-
     expect(components.length).toBeGreaterThan(0);
     expect(components).toContainEqual(
       expect.objectContaining({
         name: "SampleClass",
         filePath: expect.stringContaining("TestRepo/src/SampleClass.ts"),
+        type: "utility",
       })
     );
     expect(components).toContainEqual(
       expect.objectContaining({
         name: "SampleComponent",
         filePath: expect.stringContaining("TestRepo/src/SampleComponent.tsx"),
+        type: "component",
+      })
+    );
+    expect(components).toContainEqual(
+      expect.objectContaining({
+        name: "SampleComponent2",
+        filePath: expect.stringContaining("TestRepo/src/SampleComponent.tsx"),
+        type: "component",
+      })
+    );
+    expect(components).toContainEqual(
+      expect.objectContaining({
+        name: "SampleComponent3",
+        filePath: expect.stringContaining("TestRepo/src/SampleComponent.tsx"),
+        type: "component",
+      })
+    );
+    expect(components).toContainEqual(
+      expect.objectContaining({
+        name: "SampleComponent4",
+        filePath: expect.stringContaining("TestRepo/src/SampleComponent.tsx"),
+        type: "component",
       })
     );
   });
