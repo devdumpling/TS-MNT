170c4b37b4401a13c5e0e07afa829ee2370bf23b
Devon Wells <devon.wellsa@gmail.com>
2023-05-02 08:09:07 -0400

    feat: syntaxKindToName

---
+++ src/lib/helpers/syntaxKindToName.ts
@@ -0,0 +1,6 @@
+import * as ts from "typescript";
+
+// https://basarat.gitbook.io/typescript/overview/ast/ast-tip-syntaxkind
+export function syntaxKindToName(kind: ts.SyntaxKind) {
+  return (<any>ts).SyntaxKind[kind];
+}
